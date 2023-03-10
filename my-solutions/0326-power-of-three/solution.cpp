df318687843b7dcc2a15b0b76c0032245b2c1ac8
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-03-10 15:29:18 +0000

    Sync LeetCode submission Runtime - 19 ms (6.29%), Memory - 5.8 MB (100.00%)

---
+++ my-solutions/0326-power-of-three/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        return(isPowerOfThree(n/3)&&(n%3==0));
+    
+    }
+};
