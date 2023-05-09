d4af657698ac7b7865f68a281a127fc12feb81fb
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-05-09 06:10:17 +0000

    Sync LeetCode submission Runtime - 5 ms (0.71%), Memory - 9 MB (99.97%)

---
+++ my-solutions/0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> temp(m,0);
+        for(int i=0; i<m; i++){
+            temp[i] = nums1[i];
+        }
+        int i=0; 
+        int j=0; 
+        int k=0; 
+        while(i<m && j<n){
+            if(temp[i]<nums2[j]){
+                nums1[k] = temp[i];
+                k++;
+                i++;
+            }
+            else{
+                nums1[k] = nums2[j];
+                k++;
+                j++;
+            }
+        }
+        while(j<n){
+            nums1[k] = nums2[j];
+            k++;
+            j++;
+        }
+        while(i<m){
+            nums1[k] = temp[i];
+            k++;
+            i++;
+        }
+    }
+};
