b33c8bde272ac33f81fb849ded1cb0f20b6897b6
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-06-04 08:33:47 +0000

    Sync LeetCode submission Runtime - 50 ms (8.55%), Memory - 25.7 MB (100.00%)

--- my-solutions/0503-next-greater-element-ii/solution.cpp
+++ my-solutions/0503-next-greater-element-ii/solution.cpp
@@ -1,39 +1,71 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+
+        // declare a vector ans
         vector<int> ans;
-        vector<int> ans1;
-        stack<int> s;
-        vector<int> main;
+        // declare a vector temp of double the size than nums
+        vector<int> temp(2*nums.size(),0);
+        
+
         int n = nums.size();
-        for(int i=0; i<2*n; i++ ){
-            if(i<n){
-                main.push_back(nums[i]);
-            }
-            else if(i>=n){
-                main.push_back(nums[i-n]);
-            }
+        
+        // fill the temp with nums two times
+        // so that we can form a circular loop
+        for(int i=0; i<nums.size(); i++){
+            temp[i] = nums[i];
+            temp[nums.size()+i] = nums[i];
         }
-        for(int j=main.size()-1; j>=0; j--)
-        {
+        
+        // declare a stack
+        stack<int> st;
+        
+        // traverse from the back
+        // since we added nums two times in temp
+        // so we only put answer to our ans vector for values of i less than i
+        // because we don't want to waste memory two times
+        for(int i = 2*n-1; i>=0; i--){
             
-            while(s.empty()!=1&&main[s.top()]<=main[j]){
-                    s.pop();
+            // if the stack is empty 
+            // push the index inside the stack
+            // and push -1 into the answer array because stack is empty(if i is less
+            // than n)
+            if(st.empty()){
+                st.push(i);
+                if(i<n){
+                    ans.push_back(-1);
+                }
             }
-                    
-            if(s.empty()){
-                ans.push_back(-1);
-            }             
-                    
+
+            // check for the empty stack and if the element at the top of the stack
+            // is smaller or equal to the current element
+            // if yes then pop it out
+            while(!st.empty() && temp[i]>=temp[st.top()]){
+                st.pop();
+            }
+            
+            // if the stack is empty then push -1 (if i is less than n)
+            // push the index into the stack
+            if(st.empty()){
+                if(i<n){
+                    ans.push_back(-1);
+                }
+                st.push(i);
+            }
+
+            // else push element at the top index to the array(if it is less than n)
+            // push the index into the stack
             else{
-                ans.push_back(main[s.top()]);
+                if(i<n){
+                    ans.push_back(temp[st.top()]);
+                }
+                
+                st.push(i);
             }
-                    
-            s.push(j); 
         }
-        for(int i=2*n-1; i>=n; i--){
-             ans1.push_back(ans[i]);
-        }
-    return ans1;  
+        // reverse the answer array
+        reverse(ans.begin(),ans.end());
+        return ans;
+
     }
 };
