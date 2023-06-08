d9fc76637aba00f47fd4329e0cc18393644a50dd
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-06-08 13:11:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (99.97%)

--- my-solutions/0088-merge-sorted-array/solution.cpp
+++ my-solutions/0088-merge-sorted-array/solution.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> temp(m,0);
-        for(int i=0; i<m; i++){
-            temp[i] = nums1[i];
-        }
-        int i=0; 
-        int j=0; 
-        int k=0; 
-        while(i<m && j<n){
-            if(temp[i]<nums2[j]){
-                nums1[k] = temp[i];
-                k++;
-                i++;
+        // vector<int> temp(m,0);
+        // for(int i=0; i<m; i++){
+        //     temp[i] = nums1[i];
+        // }
+        // int i=0; 
+        // int j=0; 
+        // int k=0; 
+        // while(i<m && j<n){
+        //     if(temp[i]<nums2[j]){
+        //         nums1[k] = temp[i];
+        //         k++;
+        //         i++;
+        //     }
+        //     else{
+        //         nums1[k] = nums2[j];
+        //         k++;
+        //         j++;
+        //     }
+        // }
+        // while(j<n){
+        //     nums1[k] = nums2[j];
+        //     k++;
+        //     j++;
+        // }
+        // while(i<m){
+        //     nums1[k] = temp[i];
+        //     k++;
+        //     i++;
+        // }
+
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k--] = nums1[i--];
             }
             else{
-                nums1[k] = nums2[j];
-                k++;
-                j++;
+                nums1[k--] = nums2[j--];
             }
-        }
-        while(j<n){
-            nums1[k] = nums2[j];
-            k++;
-            j++;
-        }
-        while(i<m){
-            nums1[k] = temp[i];
-            k++;
-            i++;
         }
     }
 };
