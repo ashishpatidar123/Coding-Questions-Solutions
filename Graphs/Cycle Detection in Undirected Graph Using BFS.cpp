d373b0bbbcf726eb92571ab56bde223c3f54172b
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-06-19 19:43:19 +0530

    Create Cycle Detection in Undirected Graph Using BFS.cpp

---
+++ Graphs/Cycle Detection in Undirected Graph Using BFS.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+bool isCyclicBFS(unordered_map<int,list<int>>&adj,unordered_map<int,int>&parent,
+        unordered_map<int,bool>&visited,int node){
+        // mark the parent of the source node as -1
+        parent[node] = -1;
+        // mark it visited
+        visited[node] = true;
+        // perform the bfs traversal
+        queue<int> q;
+        q.push(node);
+
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            // traverse all the connected nodes
+            for(auto i:adj[front]){
+                // if the node is visited and is not the parent of the front
+                // then it means a cycle is present 
+                if(visited[i]==true && i!=parent[front]){
+                    return true;
+                }
+                // else check for the non visited node and push into the queue
+                else if(visited[i]==false){
+                    q.push(i);
+                    visited[i]=true;
+                    parent[i]=front;
+                }
+            }
+        }
+        return false;
+    }
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{    
+    // create a adjacency list
+    unordered_map<int,list<int>> adj;
+
+    for(int i=0; i<edges.size(); i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+
+    }
+    // create a map to track whether the node is visited or not
+    unordered_map<int,bool> visited;
+    // create a map to store the parent
+    unordered_map<int,int> parent;
+    
+    // run the loop for all the vertices
+    for(int i=1; i<=n; i++){
+        // if it is not visited then call the helper function
+        if(visited[i]==false){
+            bool ans = isCyclicBFS(adj,parent,visited,i);
+            if(ans==true){
+                return "Yes";
+            }
+        }
+    }
+    return "No";
+
+}
