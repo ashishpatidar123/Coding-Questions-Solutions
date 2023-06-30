3f587b68d629af74a627f106235926677e65f0dd
Ashish Patidar <ashishpatidar1009993@gmail.com>
2023-06-30 17:47:07 +0530

    Update Subset Sum Problem.cpp

--- Dynamic Programming./Subset Sum Problem.cpp
+++ Dynamic Programming./Subset Sum Problem.cpp
@@ -1,3 +1,4 @@
+//tabulation approach
 int Solution::solve(vector<int> &A, int B) {
     int n1 = A.size();
     vector<vector<int>>dp(n1+1,vector<int>(B+1,0));
@@ -19,3 +20,30 @@ int Solution::solve(vector<int> &A, int B) {
     }
     return dp[n1][B];
 }
+// memoisation approach
+class Solution{  
+    int solve(vector<int>&arr,int sum, int n, vector<vector<int>>&dp){
+        if(sum==0){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        if(dp[n][sum]!=-1){
+            return dp[n][sum];
+        }
+        if(arr[n-1]<=sum){
+            return dp[n][sum] = solve(arr,sum-arr[n-1],n-1,dp)||solve(arr,sum,n-1,dp);
+        }
+        else{
+            return dp[n][sum] = solve(arr,sum,n-1,dp);
+        }
+        return dp[n][sum];
+    }
+public:
+    bool isSubsetSum(vector<int>arr, int sum){
+        int n = arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        return solve(arr,sum,n,dp);
+    }
+};
