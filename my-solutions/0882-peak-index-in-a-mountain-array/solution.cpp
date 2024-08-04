d21bf6397bd78bbd9f0efb82a206132f18ec721c
Ashish Patidar <ashishpatidar1009993@gmail.com>
2024-08-04 17:51:35 +0000

    Sync LeetCode submission Runtime - 73 ms (0.48%), Memory - 62.3 MB (99.99%)

--- my-solutions/0882-peak-index-in-a-mountain-array/solution.cpp
+++ my-solutions/0882-peak-index-in-a-mountain-array/solution.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int l=0; 
-        int r = arr.size()-1;
-        int mid = (l+r)/2;
-        int ans = 0;
-        while(l<=r){
-            if(arr[mid]>arr[mid+1]){
-                ans = mid;
-                r = mid-1;
-            }
-            else if(arr[mid]<arr[mid+1]){
-                l = mid+1;
-            }
-            mid = (l+r)/2;
-            
-        }
-        return ans;
 
+        long long int left = 0;
+        long long int right = arr.size();
+        long long int n = arr.size();
+
+        while(left+1<right){
+             
+            long long int mid = left + (right-left)/2;
+
+            if(mid>0 && mid!=n-1 && arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                return mid;
+            }
+            else if(mid!=n-1 && arr[mid]<=arr[mid+1]){
+                left = mid;
+            }
+            else if(mid>0 && arr[mid]<arr[mid-1]){
+                right = mid;
+            }
+
+        }
+
+    return 0;
+        
     }
 };
