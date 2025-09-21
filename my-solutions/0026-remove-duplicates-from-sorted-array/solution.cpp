cd21f60a7dfc797ed9ccb63821218a809493cc2c
Ashish Patidar <ashishpatidar1009993@gmail.com>
2025-09-21 18:48:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.4 MB (98.27%)

--- my-solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ my-solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
-        // initiliase two pointers to index 0
-        int i = 0;
-        int j = 0;
-        // traverse the array 
-        for(int j=0; j<nums.size(); j++){
-            // if we find an element not equal to the element at the index i 
-            // then just add that element at index i+1
-            // and increase i by 1
-            if(nums[i]!=nums[j]){
-                nums[i+1] = nums[j];
-                i++;
+        int n = sizeof(nums)/sizeof(nums[0]);
+        int m = nums.size();
+        int i=0;
+        int idx = i+1;
+        int j=i+1;
+
+        while(j<m){
+
+            if(nums[i]==nums[j]){
+                j++;
             }
+            else{
+                nums[idx]=nums[j];
+                j++;
+                i = idx;
+                idx++;
+            }
+
         }
-        // return number of unique elements
-        // which is equal to last index(i) + 1
-        return i+1;
-        
+        return idx;
     }
 };
