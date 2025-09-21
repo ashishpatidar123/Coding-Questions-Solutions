65e37701d19de33586a7d0538e92e8baf66aaff1
Ashish Patidar <ashishpatidar1009993@gmail.com>
2025-09-21 15:56:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (67.30%)

--- my-solutions/0088-merge-sorted-array/solution.cpp
+++ my-solutions/0088-merge-sorted-array/solution.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // vector<int> temp(m,0);
-        // for(int i=0; i<m; i++){
-        //     temp[i] = nums1[i];
-        // }
-        // int i=0; 
-        // int j=0; 
-        // int k=0; 
-        // while(i<m && j<n){
-        //     if(temp[i]<nums2[j]){
-        //         nums1[k] = temp[i];
-        //         k++;
-        //         i++;
-        //     }
-        //     else{
-        //         nums1[k] = nums2[j];
-        //         k++;
-        //         j++;
-        //     }
-        // }
-        // while(j<n){
-        //     nums1[k] = nums2[j];
-        //     k++;
-        //     j++;
-        // }
-        // while(i<m){
-        //     nums1[k] = temp[i];
-        //     k++;
-        //     i++;
-        // }
+
 
         int i = m-1;
         int j = n-1;
         int k = m+n-1;
-        
-        while(j>=0){
-            if(i>=0 && nums1[i]>nums2[j]){
-                nums1[k--] = nums1[i--];
+        while(i>=0 || j>=0){
+
+            if(i>=0 && j>= 0){
+                if(nums1[i] < nums2[j]){
+                    nums1[k] = nums2[j];
+                    k--;
+                    j--;
+                }
+                else if(nums1[i] > nums2[j]){
+                    nums1[k] = nums1[i];
+                    i--;
+                    k--;
+                }
+                else {
+                    nums1[k] = nums1[i];
+                    k--;
+                    i--;
+                    nums1[k] = nums2[j];
+                    k--;
+                    j--;
+                }
             }
-            else{
-                nums1[k--] = nums2[j--];
+            else if(i < 0){
+                nums1[k] = nums2[j];
+                j--;
+                k--;
             }
+            else if( j<0){
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+
         }
+
+        
+        
     }
 };
