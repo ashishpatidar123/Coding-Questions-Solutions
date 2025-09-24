02bb7b917b20d90e0174c790ae20ebf0ae9bda79
Ashish Patidar <ashishpatidar1009993@gmail.com>
2025-09-24 17:18:01 +0000

    Sync LeetCode submission Runtime - 4 ms (19.28%), Memory - 28.1 MB (74.55%)

--- my-solutions/0169-majority-element/solution.cpp
+++ my-solutions/0169-majority-element/solution.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        
         int n = nums.size();
+        // solved using Boyer Moore Majority Voting
+
+        int candidate = 0;
+        int count = 0;
         int ans = 0;
-        unordered_map<int,int> m;
 
         for(int i=0; i<n; i++){
-            m[nums[i]]++;
-        }
-        for(auto i:m){
-            if(i.second > n/2){
-                ans = i.first;
-                break;
+
+            if(count == 0){
+                candidate = nums[i];
+                count = 1;
             }
+            else if(nums[i] == candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+        }
+        count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == candidate){
+                count++;
+            }
+        }
+        if(count > n/2){
+            ans = candidate;
         }
         return ans;
     }
