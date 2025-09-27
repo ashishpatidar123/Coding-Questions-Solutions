4a211f4e48b569ecc8ae26e0b36e09ffd2f82c2a
Ashish Patidar <ashishpatidar1009993@gmail.com>
2025-09-27 08:44:55 +0000

    Sync LeetCode submission Runtime - 53 ms (68.24%), Memory - 92.8 MB (63.89%)

--- my-solutions/0217-contains-duplicate/solution.cpp
+++ my-solutions/0217-contains-duplicate/solution.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        bool flag = false;
-        sort(nums.begin(),nums.end());
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]-nums[i+1]==0){
-                flag = true;
+        
+        int n = nums.size();
+        
+        unordered_map<int,int> count;
+        for(auto num : nums){
+
+            count[num]++;
+
+        }
+        for(auto it:count){
+            if(it.second > 1){
+                return true;
             }
         }
-        return flag;
+        return false;
+
     }
 };
