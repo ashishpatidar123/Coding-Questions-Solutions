4c40ea4824ab7d9551d39e94efc082f3ddb1ffb3
Ashish Patidar <ashishpatidar1009993@gmail.com>
2025-10-06 17:34:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (11.82%)

--- my-solutions/0075-sort-colors/solution.cpp
+++ my-solutions/0075-sort-colors/solution.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-    
-    int i=0;
-    int j=0;
+        
+        int n = nums.size();
+        int i = 0;
+        int j = n-1;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
 
-    while(j<nums.size()){
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                zero++;
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(i < zero){
+                nums[i] = 0;
+            }
+            else if( i >=zero && i< one+zero){
+                nums[i] = 1;
+            }
+            else{
+                nums[i] = 2;
+            }
+        }
+        
 
-        if(nums[i]>nums[j]){
-            swap(nums[i],nums[j]);
-            i++;
-            j++;
-        }
-        else if(nums[i]==nums[j]){
-            j++;
-        }
-        else{
-            i++;
-            j++;
-        }
-    }
-    i=0;
-    j=0;
-    while(j<nums.size()){
+        
 
-        if(nums[i]>nums[j]){
-            swap(nums[i],nums[j]);
-            i++;
-            j++;
-        }
-        else if(nums[i]==nums[j]){
-            j++;
-        }
-        else{
-            i=j;
-            j++;
-        }
-    }
-    
     }
 };
