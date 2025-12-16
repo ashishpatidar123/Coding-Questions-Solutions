ce7527c27845b1fb76029ba2904a37112f297feb
Ashish Patidar <ashishpatidar1009993@gmail.com>
2025-12-16 16:38:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.8 MB (57.71%)

--- my-solutions/0001-two-sum/solution.cpp
+++ my-solutions/0001-two-sum/solution.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int>m;
-        for(int i=0; i<nums.size(); i++){
-            m[nums[i]] = i;
-        }
+        int n = nums.size();
+
         vector<int>ans;
-        for(int i=0; i<nums.size(); i++){
-            int check = target - nums[i];
-            auto it = m.find(check);
-            if(it!=m.end()){
-                if(it->second!=i){
-                    ans.push_back(i);
-                    ans.push_back(it->second);
-                    return ans;
-                }
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(i!=j){
+        //             if(nums[i]+nums[j] == target){
+        //                 return {i,j};
+        //             }
+        //         }
+        //     }
+        // }
+        int left = 0;
+        int right = n-1;
+        vector<pair<int,int>>temp;
+        for(int i=0; i<n; i++){
+            temp.push_back({nums[i],i});
+        }
+        sort(temp.begin(),temp.end());
+
+
+        while(right > left){
+            int sum = temp[left].first + temp[right].first;
+
+            if(target == sum){
+                return {temp[left].second, temp[right].second};
+            }
+            else if(target > sum){
+                left++;
+            }
+            else{
+                right--;
             }
         }
-        return ans;
+
+        return {};
     }
 };
