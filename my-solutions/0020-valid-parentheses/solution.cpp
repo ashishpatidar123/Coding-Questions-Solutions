b049fc92727dd9c0a9ebdddeddae9aaec7051025
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-01-18 14:31:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (68.23%)

--- my-solutions/0020-valid-parentheses/solution.cpp
+++ my-solutions/0020-valid-parentheses/solution.cpp
@@ -1,62 +1,43 @@
 class Solution {
-    bool check(char ch1, char ch2){
-        // comparing the two brackets if they form a pair then return true 
-        // else return false
-        if(ch1=='(' && ch2==')' || ch1=='{' && ch2=='}' || ch1=='[' && ch2==']'){
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
 public:
     bool isValid(string s) {
-        // create a stack
-        stack<char> temp;
-        // find the length of the string
+        
         int n = s.length();
-        int i =0;
-        // traverse through the string
-        while(n>0){
-            // check if the stack is empty or not
-            if(temp.empty()){
-                // if it is empty then push the bracket inside the stack
-                temp.push(s[i]);
-            }
-            // else check for the upcoming bracket 
-            // if it is a closed bracket then
-            else if(s[i] == ')' || s[i]== '}' || s[i] == ']'){
-                // compare the topmost bracket inside the stack 
-                // with this upcoming bracket
-                char ch1 = temp.top();
-                char ch2 = s[i];
-                if(check(ch1,ch2)){
-                    // if it matches with the corresponding open bracket then pop out the 
-                    //top open bracket
-                    temp.pop();
+
+        stack<char>st;
+
+        int i = 0;
+
+        while(i<n){
+
+            char c = s[i];
+
+            if(!st.empty()){
+                char t = st.top();
+
+                if(c == ')' && t == '('){
+                    st.pop();
                 }
-                // else return false 
-                // which means the top bracket is either a closed bracket or a 
-                // non matching open bracket
+                else if(c == '}' && t == '{'){
+                    st.pop();
+                }
+                else if(c == ']' && t == '['){
+                    st.pop();
+                }
+                else if( c == '(' || c == '[' || c == '{'){
+                    st.push(c);
+                }
                 else{
                     return false;
                 }
             }
-            // if the upcoming bracket is a open bracket then push it inside the stack
-            else {
-                temp.push(s[i]);
+            else{
+                st.push(c);
             }
-            // update the counters
-            n--;
             i++;
+
         }
-        // now check if at then end we find our stack as empty then return true
-        if(temp.empty()){
-            return true;
-        }
-        // else return false
-        return false;
-        
+        if(!st.empty()) return false;
+        return true;
     }
 };
