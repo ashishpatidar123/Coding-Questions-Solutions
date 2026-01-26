0604d7f95ca34304a13ba5ffc6446c037eb872b5
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-01-26 10:29:34 +0000

    Sync LeetCode submission Runtime - 21 ms (60.45%), Memory - 41.8 MB (63.37%)

--- my-solutions/2856-count-complete-subarrays-in-an-array/solution.cpp
+++ my-solutions/2856-count-complete-subarrays-in-an-array/solution.cpp
@@ -11,14 +11,38 @@ public:
         int count = s.size();
         int ans = 0;
         
-        for(int i=0; i<n; i++){
-            set<int>t;
-            for(int j=i; j<n; j++){
-                t.insert(nums[j]);
-                if(t.size() == count){
-                    ans += n-j;
-                    break;
+        // for(int i=0; i<n; i++){
+        //     set<int>t;
+        //     for(int j=i; j<n; j++){
+        //         t.insert(nums[j]);
+        //         if(t.size() == count){
+        //             ans += n-j;
+        //             break;
+        //         }
+        //     }
+        // }
+        unordered_map<int,int>freq;
+
+        int l = 0;
+        int distinct = 0;
+
+        for(int r = 0; r<n; r++){
+            freq[nums[r]]++;
+            if(freq[nums[r]] == 1){
+                distinct++;
+            }
+
+            while(distinct == count){
+
+                ans += n-r;
+
+                freq[nums[l]]--;
+
+                if(freq[nums[l]] == 0){
+                    distinct--;
                 }
+                l++;
+
             }
         }
         return ans;
