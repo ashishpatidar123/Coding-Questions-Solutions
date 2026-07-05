dda32c929df8ce4006dc470160c67023ef7deb7a
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-07-05 09:01:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.4 MB (37.03%)

--- my-solutions/0547-number-of-provinces/solution.cpp
+++ my-solutions/0547-number-of-provinces/solution.cpp
@@ -1,37 +1,45 @@
 class Solution {
-private:
-    void dfs(int node, unordered_map<int,vector<int>>&adj,vector<bool>&visited){
-        visited[node]=true;
+public:
+    void dfs(int node, vector<bool>&visited, vector<vector<int>>& graph){
+        visited[node] = true;
 
-        for(auto nbr:adj[node]){
-            if(visited[nbr]==false){
-                dfs(nbr,adj,visited);
+        for(auto v : graph[node]){
+            if(!visited[v]){
+                dfs(v, visited, graph);
             }
         }
-    }
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        unordered_map<int,vector<int>> adj;
 
-        for(int i=0; i<isConnected.size(); i++){
-            for(int j=0; j<isConnected.size(); j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+        return;
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        int n = isConnected.size();
+
+        vector<vector<int>>graph(n);
+        vector<bool>visited(n, false);
+        int count = 0;
+
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int u = i;
+                int v = j;
+
+                if(isConnected[i][j] == 1){
+                    graph[u].push_back(v);
+                    graph[v].push_back(u);
                 }
             }
         }
-        int n = isConnected.size();
-        vector<bool>visited(n,false);
-        int count = 0;
 
-        for(int i=0; i<n; i++){
-            if(visited[i]==false){
-                dfs(i,adj,visited);
+        for(int u=0; u<n; u++){
+            if(!visited[u]){
+                dfs(u, visited, graph);
                 count++;
             }
         }
-        return count;
 
+        return count;
+        
     }
 };
