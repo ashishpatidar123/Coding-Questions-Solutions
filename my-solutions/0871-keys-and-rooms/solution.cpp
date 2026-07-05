578595cb2b7d458717e18a5a8bf65e4fa511e9aa
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-07-05 09:05:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.2 MB (97.35%)

--- my-solutions/0871-keys-and-rooms/solution.cpp
+++ my-solutions/0871-keys-and-rooms/solution.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    
-    void dfs(int node, vector<bool>&visited, vector<vector<int>>&rooms){
-        visited[node]=true;
+    void dfs(int node, vector<bool>& visited, vector<vector<int>>& rooms){
+        visited[node] = true;
 
-        for(auto nbr: rooms[node]){
-            if(!visited[nbr]){
-                dfs(nbr,visited,rooms);
+        for(int v : rooms[node]){
+            if(!visited[v]){
+                dfs(v, visited, rooms);
             }
         }
-        return;
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
         int n = rooms.size();
-        vector<bool>visited(n,false);
-        dfs(0,visited,rooms);
-        bool ans = false;
-        for(bool check:visited){
-            if(check==false){
+
+        vector<bool>visited(n, false);
+
+        dfs(0, visited, rooms);
+
+        for(int i=0; i<n; i++){
+            if(visited[i] == false){
                 return false;
             }
         }
+
         return true;
+
     }
 };
