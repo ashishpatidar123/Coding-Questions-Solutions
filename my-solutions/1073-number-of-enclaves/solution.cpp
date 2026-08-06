de3ca17e7f9300226c076871ecc812cae7adc5cf
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-08-06 16:57:31 +0000

    Sync LeetCode submission Runtime - 4 ms (89.68%), Memory - 28.7 MB (99.59%)

--- my-solutions/1073-number-of-enclaves/solution.cpp
+++ my-solutions/1073-number-of-enclaves/solution.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
-    vector<pair<int,int>>moves = {{1,0},{0,1},{-1,0},{0,-1}};
     int numEnclaves(vector<vector<int>>& grid) {
 
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        int total = 0;
 
         queue<pair<int,int>>q;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==1 && (i==0 || i==n-1 || j==0 || j==m-1)){
-                    visited[i][j]=true;
-                    q.push({i,j});
+        int dirs[] = {0,1,0,-1,0};
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    total ++ ;
+                    if(i == 0 || j == 0 || i == m-1 || j == n-1){
+                        q.push({i,j});
+                        grid[i][j] = 0;
+                        total--;
+                    }
                 }
             }
         }
 
         while(!q.empty()){
-            
-            pair<int,int>temp = q.front();
+            auto [r,c] = q.front();
             q.pop();
-            
-            int x = temp.first;
-            int y = temp.second;
 
-            for(int k=0; k<4; k++){
-                int i = x + moves[k].first;
-                int j = y + moves[k].second;
+            for(int l=0; l<4; l++){
+                int nr = r + dirs[l];
+                int nc = c + dirs[l+1];
 
-                if(i<n && i>=0 && j<m && j>=0 && grid[i][j]==1 && visited[i][j]==false){
-                    visited[i][j]=true;
-                    q.push({i,j});
+                if(nr >=0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
+                    grid[nr][nc] = 0;
+                    total--;
+                    q.push({nr, nc});
+
                 }
             }
-                        
+
         }
-        int count=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==1 && visited[i][j]==false){
-                    count++;
-                }
-            }
-        }
-        return count;
+        return total;
+        
     }
 };
