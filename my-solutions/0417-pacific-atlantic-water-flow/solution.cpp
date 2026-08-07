e740888e94e2ce5c95b5059494dee035a1d00132
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-08-07 17:06:06 +0000

    Sync LeetCode submission Runtime - 5 ms (67.60%), Memory - 22.6 MB (64.08%)

--- my-solutions/0417-pacific-atlantic-water-flow/solution.cpp
+++ my-solutions/0417-pacific-atlantic-water-flow/solution.cpp
@@ -1,69 +1,61 @@
 class Solution {
-    void dfs1(int i, int j,int row, int col, vector<vector<bool>>&visited, int prev,vector<vector<int>>& heights){
-        if(i<0 || i>=row || j<0 || j>=col){
-            return;
-        }
-        if(heights[i][j]<prev){
-            return;
-        }
-        if(visited[i][j]==true){
-            return;
-        }
-        visited[i][j]=true;
-        prev = heights[i][j];
-        dfs1(i+1,j,row,col,visited,prev,heights);
-        dfs1(i,j+1,row,col,visited,prev,heights);
-        dfs1(i-1,j,row,col,visited,prev,heights);
-        dfs1(i,j-1,row,col,visited,prev,heights);
-        return;
-    }
-    // void dfs2(int i, int j,int row, int col, vector<vector<bool>>&visited, int prev,vector<vector<int>>& heights){
-    //     if(i<0 || i>=row || j<0 || j>=col){
-    //         return;
-    //     }
-    //     if(heights[i][j]<prev){
-    //         return;
-    //     }
-    //     if(visited[i][j]==true){
-    //         return;
-    //     }
-    //     visited[i][j]=true;
-    //     prev = heights[i][j];
-    //     dfs2(i+1,j,row,col,visited,prev,heights);
-    //     dfs2(i,j+1,row,col,visited,prev,heights);
-    //     dfs2(i-1,j,row,col,visited,prev,heights);
-    //     dfs2(i,j-1,row,col,visited,prev,heights);
-    //     return;
-    // }
 public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int row = heights.size();
-        if(row==0){
-            return {{}};
-        }
-        int col = heights[0].size();
-        if(col==0){
-            return {{}};
-        }
-        vector<vector<bool>>pacific(row,vector<bool>(col,false));
-        vector<vector<bool>>atlantic(row,vector<bool>(col,false));
+    int m;
+    int n;
+    vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
 
-        for(int i=0; i<col; i++){
-           dfs1(0,i,row, col,pacific,heights[0][i],heights);
-           dfs1(row-1,i,row,col,atlantic,heights[row-1][i],heights);
+    void dfs(int r, int c,vector<vector<bool>>& reachable, 
+                vector<vector<int>>& heights){
+
+        reachable[r][c] = true;
+
+        for(int d=0; d<4; d++){
+            int nr = r + dirs[d][0];
+            int nc = c + dirs[d][1];
+
+            if(nr >=0 && nr < m && nc >=0 && nc < n){
+                if(!reachable[nr][nc] && heights[nr][nc] >= heights[r][c]){
+                    dfs(nr, nc, reachable, heights);
+                }
+            }
+                
         }
-        for(int i=0; i<row; i++){
-           dfs1(i,0,row, col,pacific,heights[i][0],heights);
-           dfs1(i,col-1,row,col,atlantic,heights[i][col-1],heights);
+
+        return;
+        
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+
+        m = heights.size();
+        n = heights[0].size();
+
+        vector<vector<bool>>pacific(m, vector<bool>(n, false));
+        vector<vector<bool>>atlantic(m, vector<bool>(n, false));
+
+        for(int i=0; i<m; i++){
+            dfs(i, 0, pacific, heights);
         }
-        vector<vector<int>>ans;
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(pacific[i][j]==true && atlantic[i][j]==true){
-                    ans.push_back({i,j});
+        for(int j=0; j<n; j++){
+            dfs(0, j, pacific, heights);
+        }
+        for(int i=0; i<m; i++){
+            dfs(i, n-1, atlantic, heights);
+        }
+        for(int j=0; j<n; j++){
+            dfs(m-1, j, atlantic, heights);
+        }
+
+        vector<vector<int>>result;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    result.push_back({i,j});
                 }
             }
         }
-        return ans;
+
+        return result;
+        
     }
 };
