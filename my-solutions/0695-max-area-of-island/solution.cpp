8559946949afa9983e65cafb6e8b9a1d48f70612
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-08-07 16:45:22 +0000

    Sync LeetCode submission Runtime - 4 ms (37.93%), Memory - 28.8 MB (34.42%)

--- my-solutions/0695-max-area-of-island/solution.cpp
+++ my-solutions/0695-max-area-of-island/solution.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    int n;
-    int m;
-    void dfs(int i, int j, vector<vector<bool>>&visited,vector<vector<int>>& grid, int &count){
-        visited[i][j]=true;
-        count++;
-        if(j+1<n){
-            if(visited[i][j+1]==false && grid[i][j+1]==1){
-                dfs(i,j+1,visited,grid,count);
-            }
-        }
-        if(j-1>=0){
-            if(visited[i][j-1]==false && grid[i][j-1]==1){
-                dfs(i,j-1,visited,grid,count);
-            }
-        }
-        if(i-1>=0){
-            if(visited[i-1][j]==false && grid[i-1][j]==1){
-                dfs(i-1,j,visited,grid,count);
-            }
-        }
-        if(i+1<m){
-            if(visited[i+1][j]==false && grid[i+1][j]==1){
-                dfs(i+1,j,visited,grid,count);
-            }
-        }
-        return;
-    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
 
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
-        int maxi = 0;
-    
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        queue<pair<int,int>>q;
+
+        int max_count = 0;
+
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]==1 && visited[i][j]==false){
+                if(grid[i][j]==1){
                     int count = 0;
-                    dfs(i,j,visited,grid,count);
-                    maxi = max(count,maxi);
+                    count++;
+
+                    q.push({i,j});
+                    grid[i][j]=0;
+
+                    while(!q.empty()){
+                        auto [r,c] = q.front();
+                        q.pop();
+
+                        for(int d=0; d<4; d++){
+                            int nr = r+dirs[d][0];
+                            int nc = c+dirs[d][1];
+
+                            if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
+                                grid[nr][nc] = 0;
+                                q.push({nr, nc});
+                                count++;
+                            }
+                        }
+                    }
+
+                    max_count = max(count, max_count);
                 }
             }
         }
-        return maxi;
+
+        return max_count;
+        
     }
 };
