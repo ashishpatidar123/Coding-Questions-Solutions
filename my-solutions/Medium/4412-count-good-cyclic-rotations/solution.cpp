7b46394c135744ce3e194c0bf93dbc96fd6b6390
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-09-11 00:02:37 +0530

    Implement countGoodRotations function in Solution class

---
+++ my-solutions/Medium/4412-count-good-cyclic-rotations/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countGoodRotations(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        vector<long long int>prefix(2*n+1, 0);
+        for(int i=0; i<2*n; i++){
+            prefix[i+1] = prefix[i] + nums[i%n];
+        }
+        for(int l=0; l<n; l++){
+            int m = n + l;
+            long long int last = prefix[m] - prefix[m -n/2];
+            long long int first = prefix[m - n/2] - prefix[l];
+
+            if(first > last) count++;
+        }
+        return count;
+    }
+};
