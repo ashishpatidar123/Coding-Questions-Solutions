53bf46b88e898fc9cb190af21d394f24b36743da
Ashish Patidar <ashishpatidar1009993@gmail.com>
2026-09-26 19:57:11 +0530

    Code: Add cpp solution for Largest Component Size by Common Factor

---
+++ my-solutions/Hard/0989-largest-component-size-by-common-factor/solution_2153943703.cpp
@@ -0,0 +1,81 @@
+class Solution {
+    int parent[100005];
+    int size[100005];
+
+    int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int x, int y){
+        int px = find(x);
+        int py = find(y);
+
+        if(px == py) return;
+
+        if(size[px] >= size[py]){
+            parent[py] = px;
+            size[px] += size[py];
+        }
+        else{
+            parent[px] = py;
+            size[py] += size[px];
+        }
+    }
+public:
+    int largestComponentSize(vector<int>& nums) {
+         
+        int n = nums.size();
+        int maxi = -2;
+         for(int i=0; i<n; i++){
+            maxi = max(maxi, nums[i]);
+        }
+        for(int i=0; i<=maxi; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(find(i) == find(j)){
+        //             continue;
+        //         }
+        //         if(gcd(nums[i], nums[j]) > 1){
+        //             unite(i, j);
+        //         }
+        //     }
+            
+        // }
+        // int ans = 0;
+        // for(int i=0; i<n; i++){
+        //     ans = max(ans, size[i]);
+        // }
+
+        // return ans;
+
+        for(int i=0; i<n; i++){
+            int num = nums[i];
+            // instead checking for each pair, we will 
+            // check the factors of that number and unite all
+            for(int j=2; j*j <= num; j++){
+                if(num % j == 0){
+                    unite(num, j);
+                    unite(num, num / j);
+                }
+            }
+        }
+
+        unordered_map<int, int>count;
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            // calculating the count of the numbers sharing the same root
+            int root = find(nums[i]);
+            count[root]++;
+            ans = max(ans, count[root]);
+        }
+        return ans;
+    }
+};
